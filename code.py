f778b4f02750249d19bb6a933a5722140aed9ef7
Giulio <mori.giulio.94@gmail.com>
2020-02-21 15:37:12 +0000

    ciao1

--- code.py
+++ code.py
@@ -6,6 +6,30 @@ import sys
 import numpy as np
 import pandas as pd
 
+def calculateLibraryScore(pd_total_dataset):
+    library_score = {
+        "ID_lib" : [],
+        "total_score" : [],
+        "lib_score" : [],
+    }
+
+    for idlib in pd_total_dataset.ID_lib.unique():
+        tot = pd_total_dataset[(pd_total_dataset["ID_lib"] == idlib) & (pd_total_dataset["book_done"] == 0) ].score.sum()
+        one_record = pd_total_dataset[pd_total_dataset["ID_lib"] == idlib].head(1)
+        lib_S = float(tot / (one_record['signup_days'].item() + (one_record['num_books'].item() / one_record['books_per_day'].item())))
+        library_score["ID_lib"].append(idlib)
+        library_score["total_score"].append(tot)
+        library_score["lib_score"].append(lib_S)
+    
+
+    library_dataframe = pd.DataFrame(data = library_score)
+    sorted_lib = library_dataframe.sort_values(by=["lib_score"], ascending=False)
+    return sorted_lib
+
+def updateLibDone(pd_total_dataset_loc, first_n_books_loc):
+    for idbook in list(first_n_books_loc):
+        pd_total_dataset_loc.loc[pd_total_dataset_loc['ID_book'] == idbook, 'book_done'] = 1
+    return pd_total_dataset_loc
 
 files = ['a_example.txt','b_read_on.txt','c_incunabula.txt','d_tough_choices.txt','e_so_many_books.txt','f_libraries_of_the_world.txt']
 for file_name in files:
@@ -26,17 +50,6 @@ for file_name in files:
         libraries = {}
         books = {}
         lib_i = 0
-
-        libs = {"ID_lib" : [],
-            "num_books" : [],
-            "signup_days" : [],
-            "books_per_day" : []
-            }
-
-        libs_books = {"ID_lib" : [],
-            "ID_book" : [],
-            "score" : []
-            }
 
         total_dataset = {
             "ID_lib" : [],
@@ -65,36 +78,20 @@ for file_name in files:
             lib_i += 1
 
         #library_dataframe = pd.DataFrame()
-        libs_books_dataframe = pd.DataFrame()
+        #libs_books_dataframe = pd.DataFrame()
 
         pd_total_dataset = pd.DataFrame(data = total_dataset)
         #pd_total_dataset['score'] = pd.to_numeric(pd_total_dataset['score'])
 
     #calcolo score totale potenziale di ogni libreria
-    library_score = {
-        "ID_lib" : [],
-        "total_score" : [],
-        "lib_score" : [],
-    }
-
-    for idlib in pd_total_dataset.ID_lib.unique():
-        tot = pd_total_dataset[pd_total_dataset["ID_lib"] == idlib].score.sum()
-        one_record = pd_total_dataset[pd_total_dataset["ID_lib"] == idlib].head(1)
-        lib_S = float(tot / (one_record['signup_days'].item() + (one_record['num_books'].item() / one_record['books_per_day'].item())))
-        library_score["ID_lib"].append(idlib)
-        library_score["total_score"].append(tot)
-        library_score["lib_score"].append(lib_S)
-    
-
-    library_dataframe = pd.DataFrame(data = library_score)
-    sorted_lib = library_dataframe.sort_values(by=["lib_score"], ascending=False)
+    sorted_lib = calculateLibraryScore(pd_total_dataset)
 
     output = []
     count = 0
     r_days = D
     for index, row in sorted_lib.iterrows():
         one_record = pd_total_dataset[pd_total_dataset["ID_lib"] == row['ID_lib']].head(1)
-
+        #TODO: dividere processo di signup e calcolo libri perchè mentre un libro sta in copia una libreria può fare il signup
         if r_days > one_record['signup_days'].item():
             d = {
                 'line1': [],
@@ -102,21 +99,26 @@ for file_name in files:
             }
             d['line1'].append(one_record['ID_lib'].item())
             r_days = r_days - one_record['signup_days'].item()
-            numlib = r_days * one_record['books_per_day'].item()
+            #libri fatti
+            n_books_done = pd_total_dataset[(pd_total_dataset["ID_lib"] == row['ID_lib']) & (pd_total_dataset["book_done"] == 1)].shape[0]
+            numlib = (r_days * one_record['books_per_day'].item())
             n = 0
-            if one_record['num_books'].item() < numlib:
-                d['line1'].append(one_record['num_books'].item())
-                n = one_record['num_books'].item()
+            if (one_record['num_books'].item() - n_books_done) < numlib:
+                d['line1'].append(one_record['num_books'].item() - n_books_done)
+                n = one_record['num_books'].item() - n_books_done
             else:
-                d['line1'].append(numlib)
-                n = numlib
+                d['line1'].append(numlib - n_books_done)
+                n = numlib  - n_books_done
 
-            df1 = pd_total_dataset[pd_total_dataset["ID_lib"] == one_record['ID_lib'].item()]
+            df1 = pd_total_dataset[(pd_total_dataset["ID_lib"] == row['ID_lib']) & (pd_total_dataset["book_done"] == 0)]
             df2_sorted = df1.sort_values(by=["score"], ascending=False)
             first_n_books = df2_sorted[:int(n)]['ID_book']
             d['line2'] = list(first_n_books)
             output.append(d)
             count += 1
+
+            pd_total_dataset = updateLibDone(pd_total_dataset, first_n_books)
+
 
     with open("result/result"+input_file, 'w+') as my_file:
         my_file.write(str(count) + "\n")
@@ -127,3 +129,4 @@ for file_name in files:
             for item in o['line2']:
                 my_file.write("%s " % int(item))
             my_file.write("\n")
+    break
